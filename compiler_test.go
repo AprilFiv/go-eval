633f53baf56a009ebb7a1445f5f3dbb54118c41e
Alan Li <lyx51bb@gmail.com>
2025-04-13 18:06:17 +0800

    1.unit test

--- compiler_test.go
+++ compiler_test.go
@@ -97,12 +97,12 @@ func TestDemo(t *testing.T) {
 		}
 	})
 
-	t.Run("", func(t *testing.T) {
+	t.Run("Logistic", func(t *testing.T) {
 		expressions := map[string]string{
-			"Test1": "!A",
-			"Test2": "-B",
-			"Test3": "-B+C",
-			"Test4": "-(B+C)",
+			"Test1": "A && B || C",
+			"Test2": "A && B",
+			"Test3": "A || D",
+			"Test4": "A && (B || C)",
 		}
 
 		descriptor, e := Parse(context.Background(), expressions)
@@ -111,23 +111,75 @@ func TestDemo(t *testing.T) {
 		}
 
 		c := descriptor.Context()
-
-		descriptor.Run(context.Background(), c, "A", true, nil)
-		if c.Output["Test1"].Value != false {
-			panic("1")
+		descriptor.Run(context.Background(), c, "B", false, errors.New("test error"))
+		if c.Output["Test1"] != nil {
+			panic(1)
 		}
-
-		descriptor.Run(context.Background(), c, "B", int64(10), nil)
-		if c.Output["Test2"].Value != int64(-10) {
-			panic("2")
+		if c.Output["Test2"] != nil {
+			panic(2)
 		}
-		descriptor.Run(context.Background(), c, "C", int64(10), nil)
-		if c.Output["Test3"].Value != int64(0) {
+		if c.Output["Test4"] != nil {
 			panic(3)
 		}
 
-		if c.Output["Test4"].Value != int64(-20) {
+		descriptor.Run(context.Background(), c, "C", true, nil)
+		if c.Output["Test1"].Value != true {
 			panic(4)
 		}
+		if c.Output["Test4"] != nil {
+			panic(5)
+		}
+		descriptor.Run(context.Background(), c, "A", true, nil)
+		if c.Output["Test4"].Value != true {
+			panic(3)
+		}
+		if c.Output["Test3"].Value != true {
+			panic(4)
+		}
+		if c.Output["Test2"].Err == nil {
+			panic(5)
+		}
 	})
+
+	t.Run("func", func(t *testing.T) {
+		expressions := map[string]string{
+			"Test1": "myfunc(A,B)",
+		}
+
+		expressionError := map[string]string{
+			"Test1": "myfunc2(A,B)",
+		}
+
+		resolver := WithResolver(func(funcName string) (func(ctx context.Context, args ...interface{}) (interface{}, error), error) {
+			if funcName != "myfunc" {
+				return nil, errors.New("func error")
+			}
+			return func(ctx context.Context, args ...interface{}) (interface{}, error) {
+				return args[0], nil
+			}, nil
+		})
+
+		descriptor, e := Parse(context.Background(), expressionError, resolver)
+		if e == nil {
+			panic(1)
+		}
+
+		descriptor, e = Parse(context.Background(), expressions, resolver)
+		if e != nil {
+			panic(e)
+		}
+
+		c := descriptor.Context()
+		descriptor.Run(context.Background(), c, "A", int64(5), nil)
+		if c.Output["Test1"] != nil {
+			panic(2)
+		}
+
+		descriptor.Run(context.Background(), c, "B", true, nil)
+		if c.Output["Test1"].Value != int64(5) {
+			panic(3)
+		}
+
+	})
+
 }
